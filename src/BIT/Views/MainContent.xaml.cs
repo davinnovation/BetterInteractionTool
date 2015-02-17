c9451a63547a96220ad993dfcf6da36ea5bf8d58
cho dong heon <davinnovation@gmail.com>
2015-02-17 12:08:21 +0900

    BIT uncompleted MainContent

--- src/BIT/Views/MainContent.xaml.cs
+++ src/BIT/Views/MainContent.xaml.cs
@@ -12,29 +12,17 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using MahApps.Metro.Controls;
-using BIT.Windows;
 
-namespace BIT
+namespace BIT.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xamlqazwsx
+    /// MainContent.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class MainWindow : MetroWindow
+    public partial class MainContent : UserControl
     {
-        public MainWindow()
+        public MainContent()
         {
             InitializeComponent();
-        }
-
-        private void LaunchSettings(object sender, RoutedEventArgs e)
-        {
-            new SettingsWindow() { Owner = this }.Show();
-        }
-
-        private void LaunchAbout(object sender, RoutedEventArgs e)
-        {
-            new AboutWindow() { Owner = this }.Show();
         }
     }
 }
