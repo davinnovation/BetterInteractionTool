f27fc322e918844ee1a24294d91bc83c2355290a
cho dong heon <davinnovation@gmail.com>
2015-03-23 19:56:19 +0900

    Add WindowsFunction

---
+++ src/WindowsFunctions/Get_keyboard.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Forms;
+
+//for DllImport (is DLL Import)
+using System.Runtime.InteropServices; 
+
+namespace WindowsFunctions
+{
+    class Get_keyboard
+    {
+        [DllImport("user32.dll")]
+        private static extern void keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
+
+        private const int KEYEVENTF_EXTENDEDKEY = 1;
+        private const int KEYEVENTF_KEYUP = 2;
+
+        public static void KeyDown(Keys vKey)
+        {
+            keybd_event((byte)vKey, 0, KEYEVENTF_EXTENDEDKEY, 0);
+        }
+
+        public static void KeyUp(Keys vKey)
+        {
+            keybd_event((byte)vKey, 0, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+        }
+    }
+}
---
+++ src/WindowsFunctions/Windows_function_list.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * Coded by : Davi Innovation 
+ * 
+ * list by : Microsoft - http://windows.microsoft.com/en-us/windows/keyboard-shortcuts#keyboard-shortcuts=windows-8
+ */
+
+using System.Windows.Forms;
+
+namespace WindowsFunctions
+{
+    class Windows_function_list
+    {
+        bool Switch_between_open_apps()
+        {
+            bool test = true;
+
+            try
+            {
+                Get_keyboard.KeyDown(Keys.LWin);
+                Get_keyboard.KeyDown(Keys.Tab);
+                Get_keyboard.KeyUp(Keys.LWin);
+                Get_keyboard.KeyUp(Keys.Tab);
+            }
+            catch
+            {
+                test = false;
+            }
+            return test;
+        }
+        bool Back()
+        {
+            bool test = true;
+
+            try
+            {
+                Get_keyboard.KeyDown(Keys.Alt);
+                Get_keyboard.KeyDown(Keys.Left);
+                Get_keyboard.KeyUp(Keys.Alt);
+                Get_keyboard.KeyUp(Keys.Left);
+            }
+            catch
+            {
+                test = false;
+            }
+            return test;
+        }
+    }
+}
